62f25cb7918b4e58042e25ca11765bbe598756ea
Richard Midwinter <richard.midwinter@wansdykehouse.co.uk>
2019-10-05 23:34:39 +0100

    Adding version.

--- Formula/dcash.rb
+++ Formula/dcash.rb
@@ -1,8 +1,9 @@
 class Dcash < Formula
   desc "Docker content hashing tool"
-  url "https://github.com/richmidwinter/homebrew-dcash"
+  url "https://github.com/richmidwinter/homebrew-dcash/archive/master.zip"
   head "https://github.com/richmidwinter/homebrew-dcash.git"
   sha256 "bd16c491eb8f411b14f5de653a09ac636d657974239a1b8c442b2b5be32c1dae"
+  version "1.0"
 
   def install
     bin.install "dcash"
