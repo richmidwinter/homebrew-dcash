9a76a9226bb5f5939041ab53ea9397cc867d428c
Richard Midwinter <richard.midwinter@wansdykehouse.co.uk>
2019-10-05 23:24:45 +0100

    Updating URLs.

--- Formula/dcash.rb
+++ Formula/dcash.rb
@@ -1,6 +1,7 @@
 class Dcash < Formula
   desc "Docker content hashing tool"
-  url 'https://github.com/richmidwinter/dcash.git'
+  url "https://github.com/richmidwinter/homebrew-dcash"
+  head "https://github.com/richmidwinter/homebrew-dcash.git"
 
   def install
     bin.install "dcash"
