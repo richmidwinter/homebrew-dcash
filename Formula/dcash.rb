6974cea29e5bbfcd205d3987724797dde1d033ca
Richard Midwinter <richard.midwinter@wansdykehouse.co.uk>
2019-10-05 23:26:07 +0100

    Adding SHA256.

--- Formula/dcash.rb
+++ Formula/dcash.rb
@@ -2,6 +2,7 @@ class Dcash < Formula
   desc "Docker content hashing tool"
   url "https://github.com/richmidwinter/homebrew-dcash"
   head "https://github.com/richmidwinter/homebrew-dcash.git"
+  sha256 "bd16c491eb8f411b14f5de653a09ac636d657974239a1b8c442b2b5be32c1dae"
 
   def install
     bin.install "dcash"
